cd89ed41467887594a88c4f2624dac3847e587da
bmwoolf <bradleymwoolf@gmail.com>
2025-07-09 22:22:29 -0700

    delete remote since we have local now

--- lambda_gpu/run_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python retrain_model.py --timesteps 100000 --model_name ppo_100k   | tee logs/ppo_100k.log
-python retrain_model.py --timesteps 500000 --model_name ppo_500k   | tee logs/ppo_500k.log
-python retrain_model.py --timesteps 1000000 --model_name ppo_1M    | tee logs/ppo_1M.log
-python retrain_model.py --timesteps 2000000 --model_name ppo_2M    | tee logs/ppo_2M.log
